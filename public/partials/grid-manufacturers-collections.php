2d12d5c1699978590e47402fb5da04d629ff6f1e
Juan Pablo Tort <tort.juanpablo@gmail.com>
2022-10-05 10:50:51 -0300

    Manufacturers Collections with bug pre layout change

--- public/partials/grid-manufacturers-collections.php
+++ public/partials/grid-manufacturers-collections.php
@@ -1,5 +1,5 @@
-<section class="all-products-section" style="margin-bottom:7em">
-    <h2 style="text-align: center;">Broswe Manufacturers By Collections</h2>
+<section class="all-products-section">
+    <!-- <h2 style="text-align: center;">Browse Manufacturers By Collections</h2> -->
     <?php 
     foreach ($collections as $collection) {  
         array_push($manufacturersCollections[$collection['ManufacturerId']]['collectionsArray'], array( "collectionName" => $collection['name'], "collectionID" => $collection['id']));
@@ -7,31 +7,36 @@
     //echo "<pre><code>" . print_r($manufacturersCollections) . "</code></pre>";
     ?>
     <div class="row">
-        
         <?php 
         $counter = 0;
-        $placeholder = 0;
+        // $placeholder = 0;
         $html_prefix;
         $tablinks;
         $tabs;
+        $html_prefix .= '<div class="debug-block"></div>';
         $html_prefix .= '<div class="manufacturersCollections-wrapper"><div class="tab">';
         foreach ($manufacturersCollections as $manufacturerCollection) {  
             if ( count($manufacturerCollection['collectionsArray']) >= 1 ){
                 $counter++;
-                $tablinks .=  '<a class="tablinks active" onclick="openTab(event, \'Tab' . $counter . '\')">' . $manufacturerCollection['manufacturerName']  . '</a>';
-                if ( $counter == count($manufacturersCollections)){ 
+                if (isset($_GET['showing-collecion']) && $_GET['showing-collecion'] == sanitize_title($manufacturerCollection['manufacturerName']) ) {
+                    $tablinks .=  '<a class="tablinks active" data-manufacturer="'. sanitize_title($manufacturerCollection['manufacturerName'])  .'" data-tab="Tab' . $counter . '" onclick="openTab(event, \'Tab' . $counter . '\')">' . $manufacturerCollection['manufacturerName']  . '</a>';
+                } else {
+                    $tablinks .=  '<a class="tablinks" data-manufacturer="'. sanitize_title($manufacturerCollection['manufacturerName']) .'" data-tab="Tab' . $counter . '" onclick="openTab(event, \'Tab' . $counter . '\')">' . $manufacturerCollection['manufacturerName']  . '</a>';
+                }
+                $tablinks .=  '<span> counter: ' . $counter . '</span>';
+                if ( $counter === count($manufacturersCollections)){ 
                 $tablinks .= '</div><!--/.tab-->'; 
                 } 
                 $itsCollections = $manufacturerCollection['collectionsArray'];
                 $collectionCounter = 0;
                 foreach ($itsCollections as $theCollection) {
-                    if ($placeholder == 0) {
-                        $tabs .= '<div id="TabPlaceholder" class="tabcontent active"><div class="tabcontent-wrapper"><h3>Choose a Manufacturer from the left...</h3></div><!--/.tabcontent-wrapper--></div><!--/.tabcontent-->';
-                    }
-                    $placeholder++;
+                    // if (!isset($_GET['showing-collecion']) && $placeholder == 0) {
+                    //     $tabs .= '<div id="TabPlaceholder" class="tabcontent active"><div class="tabcontent-wrapper"><h3>Choose a Manufacturer from the left...</h3></div><!--/.tabcontent-wrapper--></div><!--/.tabcontent-->';
+                    // }
+                    // $placeholder++;
                     if ( $collectionCounter == 0){ $tabs .= '<div id="Tab' . $counter . '" class="tabcontent"><div class="tabcontent-wrapper">'; }
                     $collectionCounter++;
-                    $tabs .= '<a class="tabcontent-links" href="/collection/?collection-id=' . $theCollection['collectionID'] . '">' . $theCollection['collectionName'] . '</a>';
+                    $tabs .= '<a class="tabcontent-links" href="/collection/?collection-name=' . sanitize_title($theCollection['collectionName']) . '&collection-id=' . $theCollection['collectionID'] . '">' . $theCollection['collectionName'] . '</a>';
                     if ( $collectionCounter == count($itsCollections)){ $tabs .= '</div><!--/.tabcontent-wrapper--></div><!--/.tabcontent-->'; }
                 }
             }
