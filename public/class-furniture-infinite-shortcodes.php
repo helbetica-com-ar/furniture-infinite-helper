6b6ae78aca94286320b112bc0882d232b90f1fdc
Helbetica <helbetica@outlook.com>
2023-02-20 17:09:42 -0300

    improved store name to body class preg_replace

--- public/class-furniture-infinite-shortcodes.php
+++ public/class-furniture-infinite-shortcodes.php
@@ -301,7 +301,8 @@ class Furniture_Infinite_Shortcodes
 
     public function furniture_infinite_add_body_classes($classes)
     {
-        $classes[] = str_replace(" ", "-", strtolower(get_bloginfo( 'name' )));
+        $sluggedStoreName = preg_replace("/[^A-Za-z0-9 ]/", '', get_bloginfo('name'));
+        $classes[] = str_replace(" ", "-", strtolower($sluggedStoreName));
         global $post;
         if ( is_home() || is_front_page() ) { $classes[] = 'homepage'; } 
         if ( isset( $post ) ) { $classes[] = $post->post_name; } 
