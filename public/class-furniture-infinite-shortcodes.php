6ed19b32771e8a97eed068218a71d52156458d4b
Helbetica <helbetica@outlook.com>
2023-02-20 17:17:39 -0300

    php

--- public/class-furniture-infinite-shortcodes.php
+++ public/class-furniture-infinite-shortcodes.php
@@ -301,7 +301,8 @@ class Furniture_Infinite_Shortcodes
 
     public function furniture_infinite_add_body_classes($classes)
     {
-        $classes[] = str_replace(array(" ", "\'"), "-", strtolower(get_bloginfo( 'name' )));
+        $sluggedStoreName = str_replace("'s", "-s", strtolower(get_bloginfo('name')));
+        $classes[] = str_replace(" ", "-", $sluggedStoreName);
         global $post;
         if ( is_home() || is_front_page() ) { $classes[] = 'homepage'; } 
         if ( isset( $post ) ) { $classes[] = $post->post_name; } 
