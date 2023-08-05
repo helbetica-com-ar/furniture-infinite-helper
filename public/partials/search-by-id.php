f7ce61e5a37fe7a0899ec5218ee35ee3e068308d
helbetica-com-ar <helbetica@outlook.com>
2023-08-05 02:32:04 -0300

    fixes

--- public/partials/search-by-id.php
+++ public/partials/search-by-id.php
@@ -1,11 +1,9 @@
 <h1 class="search-heading">Search</h2>
 <div class="searches-wrapper">
   <div class="search-bar-wrapper first">
-    <h2 class="search-heading"><span>Search product by</span> Furniture Item ID</h2>
-    <!--<h3 style="color:red; display: block; text-align: center;" class="search-subheading">IF YOU HAVE A EMAIL WITH THE DETAILS OF A FURNITURE IT, OR A PRINT VERSION OF IT: YOU CAN USE THE FURNTIRE ITEM ID TO LOOK IT UP AGAIN ON OWER WEBSITE</h3>-->
+    <h2 class="search-heading"><span>Search a product by</span> Furniture Item ID</h2>
   </div>
   <div class="search-bar-wrapper second">
-      <h3>Enter product ID</h3>
       <form class="search-by-product-id" method="post" action="https://<?=$_SERVER['HTTP_HOST'];?>/wp-content/plugins/furniture-infinite-helper/public/search-product-handler.php">
           <input type="number" class="field" autocomplete="off" placeholder="Enter Furniture Item ID numeric value" name="pid">
           <input type="submit" value="Search" name="submit-pid" >
