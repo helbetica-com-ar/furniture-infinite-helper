2ae3b88b51c71e0b94b3152fe5bf99fcda516f60
helbetica-com-ar <helbetica@outlook.com>
2023-08-05 02:16:04 -0300

    search by id, php and css

--- public/partials/search-by-id.php
+++ public/partials/search-by-id.php
@@ -1,18 +1,17 @@
-<h2 class="search-heading">Search Builder Name or Furniture Item by ID</h2>
-<h3 style="color:red; display: block; text-align: center;" class="search-subheading">THIS SEARCH FORMS DO NOT WORK YET | WORK IN PROGRESS</h3>
+<h1 class="search-heading">Search</h2>
 <div class="searches-wrapper">
-  <div class="search-bar-wrapper">
-      <h3>Enter Builder ID</h3>
-      <form role="form" class="search-by-builder-id" method="post" action="<?=$_SERVER['PHP_SELF'];?>">
-          <input type="number" class="field" autocomplete="off" placeholder="Enter Builder ID numeric value" name="builderId">
-          <input type="submit" value="Search">
-      </form>
+  <div class="search-bar-wrapper first">
+    <h2 class="search-heading"><span>Search Furniture Item by</span> Furniture Item by ID</h2>
+    <!--<h3 style="color:red; display: block; text-align: center;" class="search-subheading">IF YOU HAVE A EMAIL WITH THE DETAILS OF A FURNITURE IT, OR A PRINT VERSION OF IT: YOU CAN USE THE FURNTIRE ITEM ID TO LOOK IT UP AGAIN ON OWER WEBSITE</h3>-->
   </div>
-  <div class="search-bar-wrapper">
+  <div class="search-bar-wrapper second">
       <h3>Enter product ID</h3>
       <form class="search-by-product-id" method="post" action="https://<?=$_SERVER['HTTP_HOST'];?>/wp-content/plugins/furniture-infinite-helper/public/search-product-handler.php">
           <input type="number" class="field" autocomplete="off" placeholder="Enter Furniture Item ID numeric value" name="pid">
           <input type="submit" value="Search" name="submit-pid" >
       </form>
   </div>
+</div>
+<div class="img-sample-wrapper">
+    <img src="https://assets.infinitedigitalsolutions.com/media/img/furniture-item-ID-sample.png" alt="Furniture Item ID SAMPLE">    
 </div>
