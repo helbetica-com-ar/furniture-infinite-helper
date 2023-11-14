a56b4a46a0fa830e268fb0ad3b103f8f3d8fa69e
Helbetica <helbetica@outlook.com>
2023-11-14 19:39:54 +0100

    Updated template Custom Search Results Template

--- public/templates/search-results.php
+++ public/templates/search-results.php
@@ -4,21 +4,30 @@ Template Name: Custom Search Results Template
 */
 get_header();
 ?>
-<main id="content" class="site-main page page-search-results type-page status-publish hentry">
+<main id="content" class="site-main page furniture-search-results type-page status-publish hentry">
     <header class="page-header">
-        <h1 class="entry-title">Search Results</h1>
+        <h1 class="entry-title">
+            <?php
+                if ( isset($_GET['q']) && !empty($_GET['q']) ){
+                    echo 'Search Results for <span class="string-searched">' . sanitize_text_field($_GET['q']) . '</span>';
+                } else {
+                    echo 'Search';
+                }
+            ?>
+        </h1>
     </header>
     <div class="page-content">
         <div class="custom-search-wrapper">
             <?php echo do_shortcode( '[custom-search-form]' ); ?>
         </div>
-        <div class="search-results-wrapper">
+        <section class="search-results-wrapper">
         <?php
             $search_query = isset($_GET['q']) ? sanitize_text_field($_GET['q']) : '';
             if (!empty($search_query)) {
                 $url = 'https://furnitureinfinite.com/api/furniture?page=1&per_page=25&search=' . urlencode($search_query);
                 $user = FURNITURE_WP_USER;
                 $pass = FURNITURE_WP_PASS;
+                
                 // Obtain Bearer Token
                 $auth_response = wp_remote_post(FURNITURE_WP_PATH, array(
                     'headers' => array(
@@ -31,68 +40,76 @@ get_header();
                     ),
                     'sslverify' => false, // for debug only!
                 ));
+                
                 if (is_wp_error($auth_response)) {
                     echo 'Error: ' . $auth_response->get_error_message();
                     return;
                 }
+                
                 $auth_data = json_decode(wp_remote_retrieve_body($auth_response), true);
                 if (!isset($auth_data['token'])) {
                     echo 'Authentication failed. No token received.';
                     return;
                 }
+
                 $bearer_token = $auth_data['token'];
-                echo $bearer_token . '<br/>';
+                
+                // echo $bearer_token . '<br/>';
 
                 # BEARER TOKEN AUTHENTICATION GET RESPONSE WITH JSON DATA
                 $auth_header_value = "Bearer $bearer_token";
-                // $context = stream_context_create($options);
+                
                 # ENDPOINT /api/wp
                 $api_response_wp = wp_remote_get($url, array(
                     'headers' => array(
                         'Authorization' => $auth_header_value
                     )
                 ));
+
                 if ($api_response_wp === false) {
-                    echo 'Error fetching API response.';
+                    
+                    echo '<p>There was an error while fetching the information that you are looking for.</p><p>Please try again shortly</p>';
+
                     return;
+
+                } else {
+
+                    $body = wp_remote_retrieve_body($api_response_wp);
+                    
+                    echo $body;
+
+                    $data = json_decode($body, true);
+
+                    foreach ($data['data'] as $item) {
+                        if (isset($item['Images'][0]['path'])){
+                            $name = $item['name'];
+                            $pid = $item['id'];
+                            $category = $item['Category']['name'];
+                            $subcategory = $item['SubCategory']['name'];
+                            $collection = $item['Collection']['name'];
+                            $imagePath = str_replace('-original', '-300x300', $item['Images'][0]['path']);
+                            #$imagePath = $item['Images'][0]['path'];
+    
+                            echo '<article class="results-wrapper">';
+                            echo '<a href="/product-details/?from-furniture-item=' . sanitize_title($name) . '&pid=' . $pid . '">';
+                            //echo '<a href="/product-details/?pid=' . $pid . '">';
+                            echo '<img src="https://infinite-digital-production.s3.us-east-2.amazonaws.com/' . $imagePath . '" alt="' . $name . '">';
+                            echo '</a>';
+                            echo '<p class="results-title"> ' . $name . '</p>';
+                            echo '<p class="results-category">Category: ' . $category . ' » ' . $subcategory . '</p>';
+                            echo '<p class="results-collection">Collection: ' . $collection . '</p>';
+                            echo '</article>';
+                        }
+                    }
+
                 }
-                $body = wp_remote_retrieve_body($api_response_wp);
-                echo $body;
+
             } else {
-                echo '<p>No search query provided.</p>';
+                echo '<p>Type in on the search bar what you are intrested in.</p>';
             }
-
-
-
         ?>
-        </div><!-- /.search-results-wrapper -->
-        <div>
-            <?php 
-            if($bearer_token){ ?>
-                <script>
-                    jQuery(document).ready(function($) {
-                        $.ajax({
-                            url: <?php echo $url; ?>,
-                            type: 'GET',
-                            headers: {
-                                Authorization: 'Bearer ' + <?php echo $bearer_token; ?>
-                            },
-                            success: function(apiResponse) {
-                                // Handle the API response here and display results
-                                alert('YEAP!');
-                            },
-                            error: function(error) {
-                                // Handle errors
-                                alert('NOP!');
-                            }
-                        });
-                    });
-                </script>
-            <?php
-            }
-            ?>
-        </div>
+        </section><!-- /.search-results-wrapper -->
     </div><!-- /.page-content -->
 </main>
 <?php
-get_footer();
+//get_footer();
